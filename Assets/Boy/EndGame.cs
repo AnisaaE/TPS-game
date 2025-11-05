9d3badbfc1ecc81ab0397124ed8f5b29ea5c3cd3
Anisa <anisaemin97@gmail.com>
2025-11-05 15:43:54 +0300

    Added boy

---
+++ Assets/Boy/EndGame.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameTrigger : MonoBehaviour
+{
+    public Animator princeAnimator; // сложи тук аниматора на принца
+    public Animator playerAnimator; // ако искаш да пуснеш и анимация на героинята
+    public string endSceneName = "EndScene"; // следваща сцена
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Героинята намери принца 💖");
+
+            // Спиране на движението на героинята (ако имаш контролер)
+            var controller = other.GetComponent<CharacterController>();
+            if (controller != null) controller.enabled = false;
+
+            // Анимация на принца (примерно да се събуди)
+            if (princeAnimator != null)
+                princeAnimator.SetTrigger("WakeUp");
+
+            // Анимация на героинята (примерно целувка)
+            if (playerAnimator != null)
+                playerAnimator.SetTrigger("Kiss");
+
+            // След малко — край на играта
+            Invoke("EndGame", 3f);
+        }
+    }
+
+    void EndGame()
+    {
+        SceneManager.LoadScene(endSceneName);
+    }
+}
+
